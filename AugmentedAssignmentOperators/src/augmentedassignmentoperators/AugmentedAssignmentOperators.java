837b89d1d79314b684b83e85c12f15fd554ca395
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-01-26 19:05:57 +0600

    add

---
+++ AugmentedAssignmentOperators/src/augmentedassignmentoperators/AugmentedAssignmentOperators.java
@@ -0,0 +1,78 @@
+
+package augmentedassignmentoperators;
+
+import java.util.Scanner;
+
+
+public class AugmentedAssignmentOperators {
+
+    
+    public static void main(String[] args) {
+        
+//        argument ******* start
+
+//      int number=12;
+//      int number2=20;
+//      int number3=100;
+//      int number4=110;
+//      
+//      
+//        System.out.println(number+=8);
+//        System.out.println(number-=8);
+//        System.out.println(number2*=5);
+//        System.out.println(number3/=10);
+//        System.out.println(number4%3); 
+
+//                Argument *******End
+
+
+
+//                Increment******Start
+        
+//int increment=4;
+//      
+//        System.out.println(increment++);
+//        System.out.println(increment);
+//        System.out.println(increment--);
+//        System.out.println(increment);
+
+//            Increment*****End
+
+
+            impleced********start
+//
+//        System.out.println(120.566);
+//        System.out.println((int)120.566);
+                    
+//                    Impleced*****End
+//        
+//         
+
+//            ADDD***************** start
+
+Scanner number2=new Scanner(System.in);
+        System.out.println("enter add 1 :");
+    int input1=number2.nextInt();
+    
+    System.out.println("enter add 2 :");
+    int input2=number2.nextInt();
+        System.out.println("ans :"+(input1+input2));
+        
+//        Addd***************End
+//      
+//        Scanner input=new Scanner(System.in);
+//        
+//        String name;
+//
+//        System.out.println("What is your Name");
+//        name=input.nextLine();
+//        System.out.println("welcome "+name);
+//     
+        
+
+
+
+        
+    }
+    
+}
