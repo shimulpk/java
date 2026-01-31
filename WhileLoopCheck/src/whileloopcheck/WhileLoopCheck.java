7cb9ccf07a92af95a5878e64ec69111458b3424a
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-01-31 11:52:29 +0600

    add

--- WhileLoopCheck/src/whileloopcheck/WhileLoopCheck.java
+++ WhileLoopCheck/src/whileloopcheck/WhileLoopCheck.java
@@ -9,11 +9,31 @@ public class WhileLoopCheck {
     
     public static void main(String[] args) {
        
-     Scanner input=new Scanner(System.in);
-        System.out.println("Enter your name");
         
-       birthday sets
-       
+//                factorial number check
+//     Scanner input=new Scanner(System.in);
+//        System.out.println("Enter the number");
+//        int number=input.nextInt();
+//        int i=1;
+//        int count=1;
+//        while(i<=number){
+//            count*=i;
+//            i++;
+//        }
+//        System.out.println(count);
+
+
+//                do while loop
+
+            int i=5;
+            do
+            {
+                System.out.println("hi "+i);
+                i++;
+            }
+            while(i<=4);
+
+
         
         
         
