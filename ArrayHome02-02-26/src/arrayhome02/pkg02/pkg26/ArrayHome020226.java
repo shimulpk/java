0fa46dae531acac33c13ebe2a8621a3b11719a36
Md. Shimul Pramanik <shimulpk61100@gmail.com>
2026-02-03 13:50:35 +0600

    add

---
+++ ArrayHome02-02-26/src/arrayhome02/pkg02/pkg26/ArrayHome020226.java
@@ -0,0 +1,28 @@
+package arrayhome02.pkg02.pkg26;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayHome020226 {
+
+    public static void main(String[] args) {
+        
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the number of item");
+        String n=input.nextLine();
+        String [] names=new String[n];
+        double sum=0;
+        
+        System.out.println("Enter the numbers: ");
+        for(String names=0; i<names.length; i++){
+        String[i]=input.nextDouble();
+        sum+=String[i];
+        }
+        
+        
+        
+
+    }
+
+}
